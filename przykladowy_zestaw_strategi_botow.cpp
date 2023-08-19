7560082f139b9505738bc77bb82447f8c81894bf
k3chupe <94481200+k3chupe@users.noreply.github.com>
2023-08-19 13:45:21 +0200

    debugging of STRATEGY_OF_MAKING_SQUARE

--- przykladowy_zestaw_strategi_botow.cpp
+++ przykladowy_zestaw_strategi_botow.cpp
@@ -128,6 +128,7 @@ struct STRATEGY_OF_GOING_TO_THE_NEAREST_FREE_CELL : BASE{
         vector< vector< bool > >vis(HEIGHT, vector< bool >(WIDTH, false));
         POINT target_cell = bfs(player.position, map, vis);
         write_out(target_cell);
+        return;
     }
 };
 
@@ -243,6 +244,7 @@ struct STRATEGY_OF_MAKING_SQUARE : STRATEGY_OF_GOING_TO_THE_NEAREST_FREE_CELL{
     void strategy_of_making_square(PLAYER player, vector< string >& map, POINT& start, POINT& end){
         if(!am_I_on_edge(player.position, map) && no_start_square(start, end)){
             go_to_nearest_edge(player.position, map);
+            return;
         }
         if(no_start_square(start, end)){
             start = {player.position.x, player.position.y};
@@ -250,115 +252,131 @@ struct STRATEGY_OF_MAKING_SQUARE : STRATEGY_OF_GOING_TO_THE_NEAREST_FREE_CELL{
             if(end == (POINT){-1, -1}){
                 start = {-1, -1};
                 strategy_of_going_to_the_nearest_free_cell(player, map);
+                return;
             }
             write_out(end);
+            return;
         }
         if(!(player.position == end) && !(end == (POINT){-1, -1})){
             write_out(end);
+            return;
         }
         else if(player.position == end){
             end = {-1, -1};
             write_out(start);
+            return;
         }
         else if(!(player.position == start)){
             write_out(start);
+            return;
         }
         else if(player.position == start){
             start = {-1, -1};
             strategy_of_making_square(player, map, start, end);
+            return;
         }
         else{
             strategy_of_going_to_the_nearest_free_cell(player, map);
+            return;
         }
     }
 };
 
-vector <vector<int>> bfs_2(POINT& position, vector< string >& map, vector< vector< bool > >& vis){
-    vector< vector< int > >distance(HEIGHT, vector< int >(WIDTH, 0));
-    queue< POINT > q;
-    q.push(position);
-    while(!q.empty()){
-        POINT temp = q.front(); q.pop();
-        vis[ temp.x ][ temp.y ] = true;
-        for(auto[ dx, dy ] : DIRECTIONS){
-            int nx = temp.x + dx;
-            int ny = temp.y + dy;
-            if(is_on_map({nx, ny})){
-                if(!vis[ nx ][ ny ]){
-                    distance[nx][ny] = distance[temp.x][temp.y]+1; 
-                    vis[nx][ny] = true;
-                    q.push({nx, ny});
+struct STRATEGY_OF_MEARGING_BFS : STRATEGY_OF_GOING_TO_THE_NEAREST_FREE_CELL{
+
+    void start(vector< PLAYER >& players_now, vector< string >& map_now){
+        players = players_now;
+        map = map_now;
+        strategy_of_mearging_bfs(players, map);
+    }
+
+    vector <vector<int>> bfs_2(POINT& position, vector< string >& map, vector< vector< bool > >& vis){
+        vector< vector< int > >distance(HEIGHT, vector< int >(WIDTH, 0));
+        queue< POINT > q;
+        q.push(position);
+        while(!q.empty()){
+            POINT temp = q.front(); q.pop();
+            vis[ temp.x ][ temp.y ] = true;
+            for(auto[ dx, dy ] : DIRECTIONS){
+                int nx = temp.x + dx;
+                int ny = temp.y + dy;
+                if(is_on_map({nx, ny})){
+                    if(!vis[ nx ][ ny ]){
+                        distance[nx][ny] = distance[temp.x][temp.y]+1; 
+                        vis[nx][ny] = true;
+                        q.push({nx, ny});
+                    }
                 }
             }
         }
+        return distance;
     }
-    return distance;
-}
 
-vector< vector< int > > making_map_who_is_closer ( vector< PLAYER >& players, vector< string >& map){
-    vector< vector< bool > >vis(HEIGHT, vector< bool >(WIDTH, false));
-    vector<vector<int>> distance_to_me = bfs_2(players[MY_ID].position, map, vis);
-    vector< vector< bool > >vis_opo(HEIGHT, vector< bool >(WIDTH, false));
-    vector<vector<int>> distance_to_enemy = bfs_2(players[1].position, map, vis_opo);
-    vector< vector< int > >distance_map(HEIGHT, vector< int >(WIDTH, 0));
-    for (int i = 0;i < HEIGHT;i++){
-        for (int j = 0;j < WIDTH;j++){
-            int how_much = distance_to_enemy[i][j] - distance_to_me[i][j];
-            distance_map[i][j] = ((how_much <= 0)? -1 : 1) * distance_to_me[i][j];
+    vector< vector< int > > making_map_who_is_closer ( vector< PLAYER >& players, vector< string >& map){
+        vector< vector< bool > >vis(HEIGHT, vector< bool >(WIDTH, false));
+        vector<vector<int>> distance_to_me = bfs_2(players[MY_ID].position, map, vis);
+        vector< vector< bool > >vis_opo(HEIGHT, vector< bool >(WIDTH, false));
+        vector<vector<int>> distance_to_enemy = bfs_2(players[1].position, map, vis_opo);
+        vector< vector< int > >distance_map(HEIGHT, vector< int >(WIDTH, 0));
+        for (int i = 0;i < HEIGHT;i++){
+            for (int j = 0;j < WIDTH;j++){
+                int how_much = distance_to_enemy[i][j] - distance_to_me[i][j];
+                distance_map[i][j] = ((how_much <= 0)? -1 : 1) * distance_to_me[i][j];
+            }
         }
+        return distance_map;
     }
-    return distance_map;
-}
 
-void dfs(POINT n, vector< vector< bool > >& vis,vector<vector<int>>& number_of_sequence){
-    vis[n.x][n.y] = false;
-    for (POINT i : DIRECTIONS){
-        POINT u = {n.x+i.x,n.y+i.y};
-        if (is_on_map(u) == false){
-            continue;
-        }
-        if (vis[u.x][u.y] == true){
-            number_of_sequence[u.x][u.y] = number_of_sequence[n.x][n.y];
-            vis[n.x][n.y] = false;
-            dfs(u,vis,number_of_sequence);
-        }
-    }
-}
-
-vector<vector<int>> continuity(int player_number, vector< string >& map){
-    vector< vector< bool > >vis(HEIGHT, vector< bool >(WIDTH, false));
-    vector<POINT> where_path;
-    for (int i = 0;i < HEIGHT;i++){
-        for (int j = 0;j < WIDTH;j++){
-            if (map[i][j] == player_number+'0'){
-                vis[i][j] = true;
-                where_path.push_back({i,j});
+    void dfs(POINT n, vector< vector< bool > >& vis,vector<vector<int>>& number_of_sequence){
+        vis[n.x][n.y] = false;
+        for (POINT i : DIRECTIONS){
+            POINT u = {n.x+i.x,n.y+i.y};
+            if (is_on_map(u) == false){
+                continue;
+            }
+            if (vis[u.x][u.y] == true){
+                number_of_sequence[u.x][u.y] = number_of_sequence[n.x][n.y];
+                vis[n.x][n.y] = false;
+                dfs(u,vis,number_of_sequence);
             }
         }
     }
-    vector<vector<int>> number_of_sequence(HEIGHT, vector< int >(WIDTH, -3));
-    int next = 1;
-    for (POINT g : where_path){
-        if (vis[g.x][g.y]){
-            number_of_sequence[g.x][g.y] = next;
-            dfs(g,vis,number_of_sequence);
-            next++;
-        }
-    }
-    return number_of_sequence;
 
-}
-// faze alpha
-POINT decision_where_to_go( vector< PLAYER >& players, vector< string >& map,vector< vector< int > >& who_is_closer){
-    vector<vector<int>> number_of_sequence = continuity(MY_ID,map);
-    return {0,0};
-}
- 
-POINT strategy_of_mearging_bfs ( vector< PLAYER >& players, vector< string >& map){
-    vector< vector< int > > who_is_closer = making_map_who_is_closer (players, map);
-    decision_where_to_go(players, map, who_is_closer);
-    return {0,0};
-}
+    vector<vector<int>> continuity(int player_number, vector< string >& map){
+        vector< vector< bool > >vis(HEIGHT, vector< bool >(WIDTH, false));
+        vector<POINT> where_path;
+        for (int i = 0;i < HEIGHT;i++){
+            for (int j = 0;j < WIDTH;j++){
+                if (map[i][j] == player_number+'0'){
+                    vis[i][j] = true;
+                    where_path.push_back({i,j});
+                }
+            }
+        }
+        vector<vector<int>> number_of_sequence(HEIGHT, vector< int >(WIDTH, -3));
+        int next = 1;
+        for (POINT g : where_path){
+            if (vis[g.x][g.y]){
+                number_of_sequence[g.x][g.y] = next;
+                dfs(g,vis,number_of_sequence);
+                next++;
+            }
+        }
+        return number_of_sequence;
+
+    }
+    // faze alpha
+    POINT decision_where_to_go( vector< PLAYER >& players, vector< string >& map,vector< vector< int > >& who_is_closer){
+        vector<vector<int>> number_of_sequence = continuity(MY_ID,map);
+        return {0,0};
+    }
+    
+    void strategy_of_mearging_bfs ( vector< PLAYER >& players, vector< string >& map){
+        vector< vector< int > > who_is_closer = making_map_who_is_closer (players, map);
+        decision_where_to_go(players, map, who_is_closer);
+        write_out({0,0});
+    }
+};
 
 int main(){
 
@@ -386,11 +404,6 @@ int main(){
         for (int i = 0; i < HEIGHT; i++) {
             cin >> map[ i ];
         }
-        POINT output;
-        //output = strategy_of_going_to_selected_points(players[ MY_ID ], where_we_go);
-        //output = strategy_of_going_anywhere(map);
-        //output = strategy_of_going_to_the_nearest_free_cell(players[ MY_ID ], map);
-        //output = strategy_of_mearging_bfs (players, map);
         soms.start(players,map);
     }
 }
